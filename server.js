107a70ff678314e3749bf05cd1d52941080582b7
Emmanuel Izilov <em5750@gmail.com>
2021-10-26 13:34:04 -0400

    added rollbar monitoring

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const Rollbar = require('rollbar') // has to be capitalized because it's a Class
+
+let rollbar = new Rollbar({
+    accessToken: 'c21c563fcddd439ba8aa9a19924a686c',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const path = require('path')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
 })
 
 const port = process.env.PORT || 4545
