e7f5001449418445f79a50c873d519d49229940b
Emmanuel Izilov <em5750@gmail.com>
2021-10-26 13:05:28 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`Jump to hyperspace via port ${port}`))
